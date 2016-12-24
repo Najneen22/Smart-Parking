cb950b16119ff5f3b539711a4c24a34c826f10b8
daniyal <daniyal@tailoredtech.in>
2016-12-24 22:56:29 +0530

    Added login activity

---
+++ app/src/main/java/com/example/tailoredtech/smartparking/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.tailoredtech.smartparking.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tailoredtech.smartparking.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
