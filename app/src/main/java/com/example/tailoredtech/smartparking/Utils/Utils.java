92f7afe1271179b608e9c0cbe144abcff9fa61e4
Najneen <najneen@tailoredtech.in>
2016-12-24 21:59:33 +0530

    djbfds

---
+++ app/src/main/java/com/example/tailoredtech/smartparking/Utils/Utils.java
@@ -0,0 +1,34 @@
+package com.example.tailoredtech.smartparking.Utils;
+
+/**
+ * Created by Najneen on 12/24/2016.
+ */
+
+public class Utils {
+
+    /*public static void checkForInternetConnection(Context context) {
+        if (isInternetConnected(context)) {
+            syncKatCollection();
+            syncQuestions();
+            handlePrimaryFlow();
+        } else {
+            DialogUtil.showInternetMsg(SplashScreenActivity.this);
+        }
+    }
+
+    public static void showInternetMsg(final Context mContext) {
+        showDialog(mContext, Constants.NO_INTERNET_CONNECTION, new DialogUtil.IDialog() {
+            @Override
+            public void onSubmitClick() {
+                Intent intent = new Intent(Settings.ACTION_SETTINGS);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelClick() {
+
+            }
+        });
+    }*/
+}
