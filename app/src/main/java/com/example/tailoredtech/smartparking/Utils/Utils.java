6b273a0c04fe7989f7d62377b9f70f962019d646
Najneen <najneen@tailoredtech.in>
2016-12-24 22:17:41 +0530

    dfsf

--- app/src/main/java/com/example/tailoredtech/smartparking/Utils/Utils.java
+++ app/src/main/java/com/example/tailoredtech/smartparking/Utils/Utils.java
@@ -1,34 +1,26 @@
 package com.example.tailoredtech.smartparking.Utils;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+
 /**
  * Created by Najneen on 12/24/2016.
  */
 
 public class Utils {
 
-    /*public static void checkForInternetConnection(Context context) {
-        if (isInternetConnected(context)) {
-            syncKatCollection();
-            syncQuestions();
-            handlePrimaryFlow();
-        } else {
-            DialogUtil.showInternetMsg(SplashScreenActivity.this);
+    public static boolean isInternetConnected(Context context) {
+        boolean isInternetConnected = false;
+        try {
+            ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context
+                    .CONNECTIVITY_SERVICE);
+            if (connManager.getActiveNetworkInfo() != null && connManager.getActiveNetworkInfo()
+                    .isAvailable() && connManager.getActiveNetworkInfo().isConnected()) {
+                isInternetConnected = true;
+            }
+        } catch (Exception ex) {
+            isInternetConnected = false;
         }
+        return isInternetConnected;
     }
-
-    public static void showInternetMsg(final Context mContext) {
-        showDialog(mContext, Constants.NO_INTERNET_CONNECTION, new DialogUtil.IDialog() {
-            @Override
-            public void onSubmitClick() {
-                Intent intent = new Intent(Settings.ACTION_SETTINGS);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                mContext.startActivity(intent);
-            }
-
-            @Override
-            public void onCancelClick() {
-
-            }
-        });
-    }*/
 }
--- app/src/main/java/com/example/tailoredtech/smartparking/activities/SplashActivity.java
+++ app/src/main/java/com/example/tailoredtech/smartparking/activities/SplashActivity.java
@@ -1,9 +1,13 @@
 package com.example.tailoredtech.smartparking.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.tailoredtech.smartparking.R;
+import com.example.tailoredtech.smartparking.Utils.Utils;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,5 +15,20 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_actvitiy);
+
+        if(Utils.isInternetConnected(this)) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                    Intent mainIntent = new Intent(SplashActivity.this, HomeActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+            }, 1000);
+        }
+        else {
+            Toast.makeText(this, getString(R.string.no_internet_msg),Toast.LENGTH_SHORT).show();
+        }
     }
 }
