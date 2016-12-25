22a3a4963e9693513e2e1f6dd16cc71db226115d
Najneen <najneen@tailoredtech.in>
2016-12-25 08:47:39 +0530

    Added tutorial screens

--- app/src/main/java/com/example/tailoredtech/smartparking/activities/LoginActivity.java
+++ app/src/main/java/com/example/tailoredtech/smartparking/activities/LoginActivity.java
@@ -1,11 +1,17 @@
 package com.example.tailoredtech.smartparking.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.tailoredtech.smartparking.R;
 import com.viewpagerindicator.CirclePageIndicator;
@@ -21,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
 
         ViewPager mViewPager = (ViewPager)findViewById(R.id.viewpager);
         CirclePageIndicator mPageIndicator = (CirclePageIndicator) findViewById(R.id.pageIndicator);
+        mViewPager.setAdapter(new ScreenSlidePagerAdapter(this));
         mPageIndicator.setViewPager(mViewPager);
 
         btnSignIn= (Button) findViewById(R.id.btn_sign_in);
@@ -39,6 +46,48 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * A simple pager adapter that represents 2 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends PagerAdapter {
 
+        Context mContext;
+        LayoutInflater mLayoutInflater;
+        int[] mResources = {R.mipmap.tt1, R.mipmap.tt2};
+
+        public ScreenSlidePagerAdapter(Context context) {
+            mContext = context;
+            mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return mResources.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == ((LinearLayout) object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View itemView = mLayoutInflater.inflate(R.layout.pager_item, container, false);
+
+            ImageView imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            imageView.setImageResource(mResources[position]);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+            container.addView(itemView);
+
+            return itemView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((LinearLayout) object);
+        }
+    }
 }
 
