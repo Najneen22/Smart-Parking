b3a58c7ae0a67370c55673bba5a1b129d3e75812
Najneen <najneen@tailoredtech.in>
2016-12-25 08:06:20 +0530

    sdfs

--- app/src/main/java/com/example/tailoredtech/smartparking/activities/LoginActivity.java
+++ app/src/main/java/com/example/tailoredtech/smartparking/activities/LoginActivity.java
@@ -1,12 +1,14 @@
 package com.example.tailoredtech.smartparking.activities;
 
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.tailoredtech.smartparking.R;
+import com.viewpagerindicator.CirclePageIndicator;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,6 +18,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ViewPager mViewPager = (ViewPager)findViewById(R.id.viewpager);
+        CirclePageIndicator mPageIndicator = (CirclePageIndicator) findViewById(R.id.pageIndicator);
+        mPageIndicator.setViewPager(mViewPager);
+
         btnSignIn= (Button) findViewById(R.id.btn_sign_in);
         btnFbLogin= (Button) findViewById(R.id.btn_fb_login);
         btnSignIn.setOnClickListener(new View.OnClickListener() {
