ce8166473e60ac97832e1bbb346f77624f1d749f
daniyal <daniyal@tailoredtech.in>
2016-12-25 06:53:50 +0530

    avdhsd

--- app/src/main/java/com/example/tailoredtech/smartparking/adapter/BookingAdapter.java
+++ app/src/main/java/com/example/tailoredtech/smartparking/adapter/BookingAdapter.java
@@ -1,10 +1,14 @@
 package com.example.tailoredtech.smartparking.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.tailoredtech.smartparking.R;
 import com.example.tailoredtech.smartparking.models.Parking;
@@ -15,7 +19,7 @@ import java.util.ArrayList;
  * Created by Daniyal on 12/25/2016.
  */
 
-public class BookingAdapter extends RecyclerView.Adapter<BookingAdapter.MyBookingViewHolder>{
+public class BookingAdapter extends RecyclerView.Adapter<BookingAdapter.MyBookingViewHolder> {
 
     private Context mContext;
     private LayoutInflater mInflator;
@@ -24,18 +28,27 @@ public class BookingAdapter extends RecyclerView.Adapter<BookingAdapter.MyBookin
 
     public BookingAdapter(Context mContext) {
         this.mContext = mContext;
-        this.mInflator=LayoutInflater.from(mContext);
+        this.mInflator = LayoutInflater.from(mContext);
     }
 
     @Override
     public MyBookingViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view=mInflator.inflate(R.layout.list_item_book,parent,false);
+        View view = mInflator.inflate(R.layout.list_item_book, parent, false);
         return new MyBookingViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MyBookingViewHolder holder, int position) {
-
+        if(position%2==0)
+        {
+            holder.imgArrow.setBackgroundColor(ContextCompat.getColor(mContext,R.color.colorPrimary));
+        }
+        else
+        {
+            holder.imgArrow.setBackgroundColor(ContextCompat.getColor(mContext,R.color.colorPrimaryDark));
+        }
+        holder.txtCarFare.setText(mContext.getString(R.string.price, 25));
+        holder.txtBikeFare.setText(mContext.getString(R.string.price, 10));
     }
 
     @Override
@@ -44,8 +57,15 @@ public class BookingAdapter extends RecyclerView.Adapter<BookingAdapter.MyBookin
     }
 
     public class MyBookingViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtCarFare, txtBikeFare;
+        ImageView imgArrow;
+
         public MyBookingViewHolder(View itemView) {
             super(itemView);
+            txtCarFare= (TextView) itemView.findViewById(R.id.txt_fare_car);
+            txtBikeFare= (TextView) itemView.findViewById(R.id.txt_fare_bike);
+            imgArrow= (ImageView) itemView.findViewById(R.id.img_proceed);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -55,17 +75,16 @@ public class BookingAdapter extends RecyclerView.Adapter<BookingAdapter.MyBookin
         }
     }
 
-    public void updateList(ArrayList<Parking> parkings)
-    {
-        this.parkingArrayList=parkings;
+    public void updateList(ArrayList<Parking> parkings) {
+        this.parkingArrayList = parkings;
         notifyDataSetChanged();
     }
 
-    public interface IBook{
+    public interface IBook {
         void onItemClick(int position);
     }
 
-    public void setListener(IBook iBook){
-        this.iBook=iBook;
+    public void setListener(IBook iBook) {
+        this.iBook = iBook;
     }
 }
